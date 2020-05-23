a9bba9e2ce357661222cbfca27191cf8e5facfcf
TomasLlamas <35276771+TomasLlamas@users.noreply.github.com>
2020-05-23 14:45:24 -0400

    first

---
+++ public/client.js
@@ -0,0 +1,121 @@
+// initializing socket, connection to server
+var socket = io.connect("http://localhost:7777");
+socket.on("connect", function (data) {
+  socket.emit("join", "Hello server from client");
+});
+
+// listener for 'thread' event, which updates messages
+socket.on("thread", function (data) {
+  $("#thread").append("<li>" + data + "</li>");
+});
+
+// sends message to server, resets & prevents default form action
+$("form").submit(function () {
+  var message = $("#message").val();
+  socket.emit("messages", message);
+  this.reset();
+  return false;
+});
+
+
+
+function login() {
+  event.preventDefault();
+  var localName = document.getElementById("name").value;
+  var localColor = document.getElementById("color").value;
+  socket.emit("newPlayer", { color: localColor, name: localName });
+  document.getElementById("start").style.display = "none";
+}
+
+var movement = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+  kick: false,
+};
+
+document.addEventListener("keydown", function (event) {
+  switch (event.keyCode) {
+    case 37:
+      movement.left = true;
+      console.log("hi");
+      break;
+    case 38: // W
+      movement.up = true;
+      break;
+    case 39: // D
+      movement.right = true;
+      break;
+    case 40: // S
+      movement.down = true;
+      break;
+    case 32: 
+      movement.kick = true;
+      break;
+  }
+});
+document.addEventListener("keyup", function (event) {
+  switch (event.keyCode) {
+    case 37: // A
+      movement.left = false;
+      break;
+    case 38: // W
+      movement.up = false;
+      break;
+    case 39: // D
+      movement.right = false;
+      break;
+    case 40: // S
+      movement.down = false;
+      break;
+    case 32: 
+      movement.kick = false;
+      break;
+  }
+});
+
+setInterval(function () {
+  socket.emit("movement", movement);
+}, 1000 / 60);
+
+var canvas = document.getElementById("canvas");
+canvas.width = 1000;
+canvas.height = 600;
+var context = canvas.getContext("2d");
+socket.on("state", function (players) {
+  context.clearRect(0, 0, 1000, 600);
+
+
+  context.beginPath();
+  context.fillStyle = "green";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.font = "15px Arial";
+  context.textAlign = "center";
+
+  for (var id in players) {
+    var player = players[id];
+
+  if(players[id].kick === true){
+    context.beginPath();
+    context.arc(player.x, player.y, 14, 0, 2 * Math.PI);
+    context.fillStyle = "white";
+    context.fill();
+  }
+
+    context.beginPath();
+    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.fillStyle = players[id].color;
+    context.fill();
+    
+
+    context.beginPath();
+    context.fillStyle = "black";
+    context.fill();
+    context.fillText(players[id].name, player.x, player.y-15);
+    
+  }
+
+
+});
---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+
+app.get("/", function(req, res, next) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(express.static("public"));
+
+var players = {};
+
+//anything in static.js
+io.on("connection", function(client) {
+  console.log("Client connected...");
+
+  client.on("join", function(data) {
+    console.log(data);
+  });
+
+  client.on("messages", function(data) {
+    client.emit("thread", data);
+    client.broadcast.emit("thread", data);
+  });
+
+  client.on("newPlayer", function(data) {
+    players[client.id] = {
+      x: 300,
+      y: 300,
+      kick: false,
+      color: data.color,
+      name: data.name
+    };
+    console.log(players);
+  });
+  client.on('movement', function(data) {
+    var player = players[client.id] || {};
+    if (data.left) {
+      player.x -= 3;
+    }
+    if (data.up) {
+      player.y -= 3;
+    }
+    if (data.right) {
+      player.x += 3;
+    }
+    if (data.down) {
+      player.y += 3;
+    }
+    if (data.kick) {
+      player.kick = true;
+    } else {
+      player.kick = false;
+    }
+  });
+  client.on('disconnect', function() {
+    delete players[client.id];
+  });
+});
+
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
+
+//running on 7777...
+server.listen(7777);
+
